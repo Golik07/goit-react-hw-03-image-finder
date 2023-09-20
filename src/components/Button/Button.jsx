0b466d3a3b6b809dd853d900c4bd66ceb69a2085
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-09-20 21:40:39 +0200

    update

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import css from "./Button.module.css"
+import PropTypes from "prop-types"
 
 
 const Button = ({click}) => {
@@ -10,3 +11,7 @@ const Button = ({click}) => {
 }
 
 export default Button;
+
+Button.propTypes = {
+    click: PropTypes.func.isRequired,
+}
